8917f452257b52f6e6c3495ca8981edfff54cf44
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2020-08-18 22:07:14 +1200

    Moved dnnet under the 'maths' module

--- src/maths/dennet.py
+++ src/maths/dennet.py
@@ -2,32 +2,31 @@ import torch
 import torch.nn as nn
 import torchdiffeq
 import pytorch_lightning as pl
-from .diffeq import DiffEq
+from maths.diffeq import DiffEq
 
 
 class DENNet(pl.LightningModule):
     """
     A class to handle lower-level parameters of a differential equation neural network.
-    This class inherits from PyTorch-Lightning
+    This class inherits from PyTorch-Lightning.
     """
 
     def __init__(self, func: nn.Module, order=1, sensitivity='autograd', s_span=torch.linspace(0, 1, 2), solver='rk4',
                  atol=1e-4, rtol=1e-4):
-
         super().__init__()
 
         # have an if for DiffEq of PDEeq
         self.de_function = DiffEq(func, order)
         self.order = order
         self.sensitivity = sensitivity
-        self.s_span = s_span
+        self.t_span = s_span
         self.solver = solver
         self.nfe = self.de_function.nfe
         self.rtol = rtol
         self.atol = atol
 
     def _prep_odeint(self, x: torch.Tensor):
-        self.s_span = self.s_span.to(x)
+        self.t_span = self.t_span.to(x)
 
         # TODO handle other named_modules
 
@@ -56,7 +55,7 @@ class DENNet(pl.LightningModule):
     def _autograd(self, x):
         self.de_function.sensitivity = self.sensitivity
 
-        return torchdiffeq.odeint(self.de_function, x, self.s_span, rtol=self.rtol, atol=self.atol,
+        return torchdiffeq.odeint(self.de_function, x, self.t_span, rtol=self.rtol, atol=self.atol,
                                   method=self.solver)[-1]
 
     @property
